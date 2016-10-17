65aa2a2eb7655fdda47b6f56c2f6dd113a7bdb09
David Gaynor <runnedrun@gmail.com>
2016-10-17 11:30:23 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,25 @@
+var person = {
+  name: "David",
+  representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
+  height: "500px",	
+  favoriteColor: "blue",
+  catchphrase: "Get pumped!"
+}
+
+drawPerson(person)
+var y = 9
+console.log("ASdfasF")
+console.log("asdf")
+console.log("asdf")
+console.log("asdf")
+console.log("asdf")
+ssss
+s
+asdfasdf
+asdf
+
+
+
+function drawPerson(person) {
+  person.name = "Ralph"
+}
