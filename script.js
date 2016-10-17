1991f587e4c7638a046f07a51d6168b5c43387fb
David Gaynor <runnedrun@gmail.com>
2016-10-17 10:11:14 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -12,7 +12,48 @@ console.log("asdf")console.log("asdf")
 console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")console.log("asdf")
 aslkdjfasdf
  as
- df asdflk jas;dlkf 
+ df asdflk jas;dlkf ;laksdf;lk asd;lf ;alsdkf;lkasdf;lkasd;lk
+ asd;lkas
+ as;lkasd;lfk;lkasdfasdf;laskd;fkals;dfkasdfkasldfkas;dfl;kasd
+ as;lkdf;klasdl;fas;dlfa
+ sd
+ ;lfk;askdflkasd;fa
+ sdfasdl;fkasd
+ f;kas
+ dlkf;alskdf
+ l;asl;dl;kdld
+ d
+ dldldlddl
+ d
+ d
+ d
+ sa
+ fd
+ asd
+ fa
+ sd
+ fasdgasdg
+ sf
+ ga
+ f
+ dsagf
+ dhfgsdaffdghjkf
+ dzsghdj
+ sda
+ gh
+ sa
+ gh
+ sa
+ gh
+ sa
+ gh
+ jd
+ s
+ ghj
+ fds
+ a
+ ghjs
+ console.log("ASdf")
 
 function drawPerson(person) {
   person.name = "Ralph"
